99e09e75ed08b2bce0ac14dded053a23939aa557
Ellie <dpfla9144@naver.com>
2023-03-13 11:36:13 +0900

    cleaning code

--- scripts/getData.js
+++ scripts/getData.js
@@ -45,64 +45,25 @@ fetch("../data/appData.json")
     return response.json();
   })
   .then(function (data) {
-    // 가져온 data array의 object를 하나씩 꺼낸다.
-    // 가져온 object의 카테고리와 core를 변수에 저장한다.
-    // 카테고리와 동일한 카테고리 element를 찾는다.
-
-    // core array의 string들을 꺼낸다.
-    // array의 string은 function name이고, nextElementSibling인 ol에 이미 동일한 innerText를 가진 li > a가 존재하는지 확인한다.
-    // 동일한 list가 이미 존재하면 추가하지 않는다.
-    // 존재하지 않는다면 그 element의 nextElementSibling인 ol에 a를 포함하는 list를 추가한다.
-
     for (const object of data) {
+      // 가져온 data array의 object를 하나씩 가져와 카테고리와 core를 변수에 저장한다.
       const objectCategory = object.category;
       const objectFunctionNames = object.core;
 
+      // 카테고리와 동일한 카테고리 element를 찾는다.
       const categoryH3Element = findSameCategory(objectCategory);
 
+      // core array의 string들을 꺼낸다.
       for (const objectFunctionName of objectFunctionNames) {
+        // array의 string은 function name이고, nextElementSibling인 ol에 이미 동일한 innerText를 가진 li > a가 존재하는지 확인한다.
         const isSameListResult = isSameListInCategory(
           categoryH3Element,
           objectFunctionName
         );
+        // 동일한 list가 이미 존재하지 않는다면 해당 functionName을 가지는 list를 추가한다.
         if (isSameListResult == 0) {
           addList(categoryH3Element, objectFunctionName);
         }
       }
     }
-
-    // if category and data's category, then add data's content behind of that category
-
-    // for (const item of data) {
-    //   appData.push(item);
-    //   for (const appItem of categoryElements) {
-    //     // if category and data's category, then add data's content behind of that category
-    //     if (item.category == appItem.innerText) {
-    //       for (const core of item.core) {
-    //         const newList = document.createElement("li");
-    //         const newAnchor = document.createElement("a");
-    //         const newAttribute = document.createAttribute("href");
-
-    //         newAnchor.innerText = core;
-    //         newAttribute.value = `../function.html#${core}`;
-
-    //         const functionNameElements = document.querySelectorAll(
-    //           ".category-wrap ol li"
-    //         );
-    //         for (const element of functionNameElements) {
-    //           console.log(element);
-
-    //           // if (element.innerText != core) {
-    //           //   // 이미 만들어진 리스트 중에서 동일한 기능이 있다면 추가하지 않고 다음 core로 넘어간다.
-    //           //   console.log(element);
-
-    //           //   appItem.nextElementSibling.appendChild(newList);
-    //           //   newList.appendChild(newAnchor);
-    //           //   newAnchor.setAttributeNode(newAttribute);
-    //           // }
-    //         }
-    //       }
-    //     }
-    //   }
-    // }
   });
