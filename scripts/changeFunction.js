f1ccdd18f0f7c2cdfa8923fc3b50c9266faffba1
Ellie <dpfla9144@naver.com>
2023-03-15 14:46:53 +0900

    change detailFunction.html's js changeFunction.js due to deviding app.json and function.json

--- scripts/changeFunction.js
+++ scripts/changeFunction.js
@@ -1,33 +1,32 @@
 const h1Element = document.querySelector("h1");
-const appCharacteristicElement = document.getElementById(
+const functionCharacteristicElement = document.getElementById(
   "description-characteristic"
 );
 const ulElementForAppList = document.getElementById("list-ul");
-const appFunctionCoreElement = document.querySelector("#use ul");
+const subfunctionElements = document.querySelector("#main-main ul");
 
-const appData = new Array();
+const functionObjectArray = new Array();
 const url = decodeURI(window.location.href);
 const functionName = url.substring(url.indexOf("#") + 1, url.length);
 
-function getFunctionObjectInArray(array) {
-  for (const object of array) {
-    const functionNameArray = object.functions; // object.functions is array and contain objects
-    for (const functionNameItem of functionNameArray) {
-      if (functionNameItem == functionName) {
-        appData.push(object);
-      }
+function getFunctionObjectInArray(functionData) {
+  for (const functionObjectData of functionData) {
+    const functionObjectName = functionObjectData.name; // object.functions is array and contain objects
+    if (functionName == functionObjectName) {
+      functionObjectArray.push(functionObjectData);
+      return;
     }
   }
 }
 
-function makeAppList(data) {
-  for (const object of appData) {
+function makeAppList() {
+  for (const appName of functionObjectArray[0].app) {
     const newList = document.createElement("li");
     const newAnchor = document.createElement("a");
     const newAttribute = document.createAttribute("href");
 
-    newAnchor.innerText = object.name;
-    newAttribute.value = `./app.html#${object.name}`;
+    newAnchor.innerText = appName;
+    newAttribute.value = `./app.html#${appName}`;
 
     ulElementForAppList.appendChild(newList);
     newList.appendChild(newAnchor);
@@ -39,17 +38,17 @@ function changeHTML() {
   //change html text using object data
   h1Element.innerText = `${functionName} 기능 소개`;
   makeAppList();
-  // appCharacteristicElement.innerText = appData[0].특징;
+  // appCharacteristicElement.innerText = functionData[0].특징;
   // console.dir();
 }
 
 // read local JSON file in javascript
-fetch("../data/app.json")
+fetch("../data/function.json")
   .then(function (response) {
     return response.json();
   })
-  .then(function (data) {
-    getFunctionObjectInArray(data);
-    console.log(appData);
+  .then(function (functionData) {
+    getFunctionObjectInArray(functionData);
+    console.log(functionObjectArray);
     changeHTML();
   });
