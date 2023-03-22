668646c4811967fc436d5ff3f6d1779dd9832523
Ellie <dpfla9144@naver.com>
2023-03-22 12:28:23 +0900

    add js code for change display box when i click the description lists

--- scripts/display.js
+++ scripts/display.js
@@ -1,18 +1,26 @@
 const methodListElement = document.querySelectorAll("#main-main-wrap ol li");
+const displayBox1Element = document.getElementById("display-box1");
+const displayBox2Element = document.getElementById("display-box2");
+const displayBox3Element = document.getElementById("display-box3");
 
 for (const liElement of methodListElement) {
   liElement.addEventListener("click", (data) => {
     const appData = data.target.dataset.method;
-
     switch (appData) {
       case "전화어플 누른다":
-        console.log(appData);
+        displayBox1Element.classList.add("none");
+        displayBox3Element.classList.add("none");
+        displayBox1Element.classList.remove("none");
         break;
       case "전화번호 누른다":
-        console.log(appData);
+        displayBox1Element.classList.add("none");
+        displayBox3Element.classList.add("none");
+        displayBox2Element.classList.remove("none");
         break;
       case "통화버튼 누른다":
-        console.log(appData);
+        displayBox1Element.classList.add("none");
+        displayBox2Element.classList.add("none");
+        displayBox3Element.classList.remove("none");
         break;
       default:
         console.log(`Sorry, we are out of ${appData}.`);
