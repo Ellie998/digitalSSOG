8adc67f4ce59fc5bc70b2179c353e9fe2a7704b0
Ellie <dpfla9144@naver.com>
2023-03-22 12:20:50 +0900

    make displays for description

--- scripts/display.js
+++ scripts/display.js
@@ -2,6 +2,20 @@ const methodListElement = document.querySelectorAll("#main-main-wrap ol li");
 
 for (const liElement of methodListElement) {
   liElement.addEventListener("click", (data) => {
-    console.dir(data.target.dataset.method);
+    const appData = data.target.dataset.method;
+
+    switch (appData) {
+      case "전화어플 누른다":
+        console.log(appData);
+        break;
+      case "전화번호 누른다":
+        console.log(appData);
+        break;
+      case "통화버튼 누른다":
+        console.log(appData);
+        break;
+      default:
+        console.log(`Sorry, we are out of ${appData}.`);
+    }
   });
 }
