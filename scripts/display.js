69f8c4defc110ffee961ec47d9e0733764fc53d8
Ellie <dpfla9144@naver.com>
2023-03-22 12:45:34 +0900

    add js code for change display box when i click display target btn

--- scripts/display.js
+++ scripts/display.js
@@ -1,10 +1,35 @@
-const methodListElement = document.querySelectorAll("#main-main-wrap ol li");
+const methodListElements = document.querySelectorAll("#main-main-wrap ol li");
+const targetMethodElements = document.querySelectorAll(".target");
 const displayBox1Element = document.getElementById("display-box1");
 const displayBox2Element = document.getElementById("display-box2");
 const displayBox3Element = document.getElementById("display-box3");
 
-for (const liElement of methodListElement) {
+for (const liElement of methodListElements) {
   liElement.addEventListener("click", (data) => {
+    const appData = data.target.dataset.method;
+    switch (appData) {
+      case "전화어플 누른다":
+        displayBox2Element.classList.add("none");
+        displayBox3Element.classList.add("none");
+        displayBox1Element.classList.remove("none");
+        break;
+      case "전화번호 누른다":
+        displayBox1Element.classList.add("none");
+        displayBox3Element.classList.add("none");
+        displayBox2Element.classList.remove("none");
+        break;
+      case "통화버튼 누른다":
+        displayBox1Element.classList.add("none");
+        displayBox2Element.classList.add("none");
+        displayBox3Element.classList.remove("none");
+        break;
+      default:
+        console.log(`Sorry, we are out of ${appData}.`);
+    }
+  });
+}
+for (const targetMethodElement of targetMethodElements) {
+  targetMethodElement.addEventListener("click", (data) => {
     const appData = data.target.dataset.method;
     switch (appData) {
       case "전화어플 누른다":
